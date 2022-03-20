be15e62a74b842ff6769423feb5d92535b8b02b9
musiermoore <musiermoore@gmail.com>
2022-03-20 18:07:26 +0600

    Created state variable for the main loader of the page

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,24 @@ import {
 const store = createStore({
     state () {
         return {
-            user: null
+            user: null,
+            mainLoaded: false
         }
     },
     mutations: {
         user (state, user) {
             state.user = user
+        },
+        mainLoaded (state, status) {
+            state.mainLoaded = status
         }
     },
     getters: {
         user (state) {
             return state.user
+        },
+        mainLoaded (state) {
+            return state.mainLoaded
         }
     }
 })
