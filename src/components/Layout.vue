ffe9f7e1e5267a6ad4c9e280a1c8fca808728750
musiermoore <musiermoore@gmail.com>
2022-03-20 18:07:38 +0600

    Created main layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="container-fluid flex-grow-1 d-flex bg-lighter">
+    <div class="row flex-grow-1">
+      <navbar />
+
+      <section>
+        <div class="container">
+          <slot></slot>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+
+export default {
+  name: "Layout",
+  components: {
+    navbar: Navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
