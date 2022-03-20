b8550ced30097f05470a020d9ad8703663cd97a3
musiermoore <musiermoore@gmail.com>
2022-03-20 18:08:53 +0600

    Added initialize api (axios), authorize user if the token exists and main loader

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,88 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <loader v-if="!loaded" />
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <script>
+import { getCurrentInstance } from "vue"
+import axios from "axios"
+import PageLoader from "./components/Loaders/PageLoader"
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    'loader': PageLoader
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    initApi() {
+      const { appContext } = getCurrentInstance()
+
+      appContext.config.globalProperties.$api = process.env.VUE_APP_BASE_URL
+
+      const base = axios.create({
+        baseURL: this.$api,
+      })
+
+      appContext.config.globalProperties.$axios = base
+      appContext.config.globalProperties.$axios.interceptors.request.use(
+          config => {
+            let accessToken = localStorage.getItem('token')
+            if (accessToken) {
+              config.headers = Object.assign({
+                Authorization: `Bearer ${accessToken}`
+              }, config.headers);
+            }
+            return config
+          },
+          error => {
+            return Promise.reject(error)
+          }
+      )
+    },
+    authenticateUser() {
+      const token = localStorage.getItem('token')
+
+      if (this.user || !token) {
+        this.$store.commit('mainLoaded', true)
+        return;
+      }
+
+      this.$axios.get('/auth/user')
+        .then((response) => {
+          if (response.status === 200) {
+            const user = response.data.data.user
+
+            this.$store.commit('user', user)
+          } else {
+            localStorage.removeItem('token')
+            this.$store.commit('user', null)
+            this.$router.push({ name: 'Login' })
+          }
+        })
+        .finally(() => this.$store.commit('mainLoaded', true))
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.getters.user
+    },
+    loaded() {
+      return this.$store.getters.mainLoaded
+    }
+  },
+  mounted() {
+    this.initApi()
+    this.authenticateUser()
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~bootstrap/dist/css/bootstrap.css';
 </style>
