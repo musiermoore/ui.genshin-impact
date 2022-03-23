9535136a1e3ec54f807408fec5156d29197ead9b
musiermoore <musiermoore@gmail.com>
2022-03-23 21:14:34 +0600

    Created admin character pages

---
+++ src/views/Character/Admin/CharacterCreate.vue
@@ -0,0 +1,147 @@
+<template>
+  <Layout>
+    <h1>Создание персонажа</h1>
+    <form class="col-12 col-md-4" @submit.prevent="createCharacter" enctype='multipart/form-data'>
+      <div class="mb-3">
+        <label for="name" class="form-label">Имя персонажа:</label>
+        <input type="text" class="form-control" id="name" v-model="characterData.name" required>
+      </div>
+      <div class="mb-3">
+        <label for="slug" class="form-label">Уникальный идентификатор</label>
+        <input type="text" class="form-control" id="slug" v-model="characterData.slug">
+        <small>Оставить пустым или имя персонажа на английском</small>
+      </div>
+      <div class="mb-3">
+        <label for="stars" class="form-label">Редкость:</label>
+        <select
+            class="form-select"
+            name="star_id"
+            id="stars"
+            v-model="characterData.star_id"
+            required
+        >
+          <option value="" disabled selected>Выберите звездочку</option>
+          <option v-for="star in stars" :key="star.id" :value="star.star">
+            {{ star.star }}
+          </option>
+        </select>
+      </div>
+
+      <div class="mb-3">
+        <label for="elements" class="form-label">Стихия:</label>
+        <select
+            class="form-select"
+            name="star_id"
+            id="elements"
+            v-model="characterData.element_id"
+            required
+        >
+          <option value="" disabled selected>Выберите стихию</option>
+          <option v-for="element in elements" :key="element.id" :value="element.id">
+            {{ element.element }}
+          </option>
+        </select>
+      </div>
+
+      <div class="mb-3">
+        <label for="stars" class="form-label">Тип оружия:</label>
+        <select
+            class="form-select"
+            name="weapon_type_id"
+            id="weapon_types"
+            v-model="characterData.weapon_type_id"
+            required
+        >
+          <option value="" disabled selected>Выберите тип оружия</option>
+          <option v-for="weaponType in weaponTypes" :key="weaponType.id" :value="weaponType.id">
+            {{ weaponType.type }}
+          </option>
+        </select>
+      </div>
+
+      <div class="mb-3">
+        <label for="image" class="form-label">Изображение</label>
+        <input class="form-control" type="file" id="image" required @change="onFileChange">
+      </div>
+
+      <div class="mb-3">
+        <button
+            type="submit"
+            class="btn btn-primary"
+            id="submit"
+        >Создать</button>
+      </div>
+    </form>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/components/Layout";
+
+export default {
+  name: "CharacterCreate",
+  components: {
+    Layout
+  },
+  data() {
+    return {
+      characterData: {
+        name: 'Test',
+        star_id: 5,
+        element_id: 3,
+        weapon_type_id: 2
+      },
+      stars: [],
+      elements: [],
+      weaponTypes: []
+    }
+  },
+  mounted() {
+    this.getBasicData()
+  },
+  methods: {
+    createCharacter() {
+      const formData = new FormData()
+      const data = this.characterData
+
+      const headers = {
+        // "Content-Type": "multipart/form-data"
+      }
+
+      for (let key in data) {
+        formData.append(key, data[key]);
+      }
+
+      this.$axios.post('/admin/characters', formData, headers)
+          .then((response) => {
+            if (response.status === 200) {
+              this.$router.push({ name: 'Characters' })
+            }
+          })
+          .finally(() => this.loaded = true)
+    },
+    getBasicData() {
+      this.$axios.get('/admin/characters/create')
+          .then((response) => {
+            const data = response.data.data
+
+            this.stars = data.stars
+            this.weaponTypes = data.weapon_types
+            this.elements = data.elements
+          })
+          .finally(() => this.loaded = true)
+    },
+    onFileChange(event) {
+      const files = event.target.files || event.dataTransfer.files;
+
+      if (!files.length) return;
+
+      this.characterData.image = files[0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Character/List/Characters.vue
+++ src/views/Character/List/Characters.vue
@@ -1,7 +1,32 @@
 <template>
   <layout>
-    <div class="characters">
-      Персонажи
+    <div class="d-flex justify-content-between align-items-center mb-3">
+      <h1>Персонажи:</h1>
+      <div>
+        <router-link
+            :to="{ name: 'Create Character' }"
+            class="btn btn-primary"
+            tag="button"
+        >
+          Создать персонажа
+        </router-link>
+      </div>
+
+    </div>
+
+    <div class="characters d-flex flex-wrap">
+      <div class="character" v-for="character in characters" :key="character.id">
+        <div class="character-image-block">
+          <img
+              :src="getCharacterImage(character)"
+              :alt="character.name"
+              class="character-image"
+          >
+        </div>
+        <div class="character-info">
+          {{ character.name }}
+        </div>
+      </div>
     </div>
   </layout>
 </template>
@@ -13,10 +38,66 @@ export default {
   name: "Characters",
   components: {
     'layout': Layout
+  },
+  data() {
+    return {
+      characters: [],
+      loaded: false
+    }
+  },
+  methods: {
+    getCharacters() {
+      this.$axios.get('/admin/characters')
+          .then((response) => {
+            if (response.status === 200) {
+              this.characters = response.data.data.characters
+            }
+          })
+          .finally(() => this.loaded = true)
+    },
+    getCharacterImage(character) {
+      const images = character.images
+
+      const mainImage = images.find(image => image.image_type.slug === 'main')
+
+
+      return mainImage
+          ? `${this.$storageUrl}/${mainImage.path}`
+          : null
+    }
+  },
+  mounted() {
+    this.getCharacters()
   }
 }
 </script>
 
 <style scoped>
+.character {
+  width: 200px;
+  height: 200px;
+  position: relative;
+  margin-right: 20px;
+}
+.character:last-of-type {
+  margin-right: 0;
+}
+
+.character-image-block {
+  width: 200px;
+  height: 200px;
+  position: relative;
+  border: 2px solid #666;
+}
+.character-image {
+  width: 198px;
+  height: 198px;
+  padding: 0;
+  margin: 0;
+}
+.character-info {
+  display: flex;
+  justify-content: center;
+}
 
 </style>
