085e3fb2f230ab9876869f37d63e9db541cef8bf
musiermoore <markowillz@gmail.com>
2022-10-16 16:54:52 +0600

    GG-3: Created a mixin to get base artifact information

---
+++ src/mixins/Calculator/getBaseArtifactInformation.js
@@ -0,0 +1,23 @@
+export default {
+    methods: {
+        getArtifactTypes() {
+            return [
+                {
+                    slug: 'flower-of-life'
+                },
+                {
+                    slug: 'plume-of-death'
+                },
+                {
+                    slug: 'sands-of-eon'
+                },
+                {
+                    slug: 'goblet-of-eonothem'
+                },
+                {
+                    slug: 'circlet-of-logos'
+                },
+            ]
+        }
+    }
+}
